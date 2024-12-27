62cce7a2fef635f5f9a8299c5fdc50f47fe574fc
Kim Haejin <myjin0806@gmail.com>
2024-12-27 14:30:25 +0900

    update Header, debugging

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,51 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const header = () => {
+const Header = () => {
   return (
     <header>
       <div className="headerWrap">
-        <NavLink to='/' className="logo">FRONT-END * 김해진</NavLink>
+        <NavLink to="/" className="logo">FRONT-END * 김해진</NavLink>
         <nav>
           <ul>
             <li>
-              <NavLink to="/" className="navLink home">
-                <img src='/images/home.png' alt="Home Icon" />
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? 'navLink home active' : 'navLink home')}
+                end
+              >
+                <img src="/images/home.png" alt="Home Icon" />
               </NavLink>
             </li>
             <li>
-              <NavLink to="/about" className="navLink about">
-                <img src='/images/about.png' alt="About Icon" />
+              <NavLink
+                to="/about"
+                className={({ isActive }) => (isActive ? 'navLink about active' : 'navLink about')}
+              >
+                <img src="/images/about.png" alt="About Icon" />
               </NavLink>
             </li>
             <li>
-              <NavLink to="/portfolio" className="navLink portfolio">
-                <img src='/images/portfolio.png' alt="Portfolio Icon" />
+              <NavLink
+                to="/portfolio"
+                className={({ isActive }) => (isActive ? 'navLink portfolio active' : 'navLink portfolio')}
+              >
+                <img src="/images/portfolio.png" alt="Portfolio Icon" />
               </NavLink>
             </li>
             <li>
-              <NavLink to="/contect" className="navLink contect">
-                <img src='/images/contect.png' alt="Contect Icon" />
+              <NavLink
+                to="/contect"
+                className={({ isActive }) => (isActive ? 'navLink contect active' : 'navLink contect')}
+              >
+                <img src="/images/contect.png" alt="Contect Icon" />
               </NavLink>
             </li>
           </ul>
         </nav>
       </div>
-
     </header>
-  )
-}
+  );
+};
 
-export default header
+export default Header;
